b880fba120c8aca75aae17a5bb19ed5a263bec48
Nick Freear <nfreear@yahoo.co.uk>
2019-06-02 23:13:44 +0100

    Add prettier, eslint and travis YAML; lint fixes

--- src/script.js
+++ src/script.js
@@ -9,15 +9,15 @@ const MarkdownIt = require('markdown-it')({
   html: true,
   linkify: true,
   typographer: true
-})
+});
 
-const $PRE = DOC.querySelector('pre')
+const $PRE = DOC.querySelector('pre');
 
-const $DEST = DOC.createElement('div')
-const $STYLE = DOC.createElement('link')
+const $DEST = DOC.createElement('div');
+const $STYLE = DOC.createElement('link');
 
 const SRC = $PRE.innerText;
-const HTML = MarkdownIt.render(SRC)
+const HTML = MarkdownIt.render(SRC);
 
 $DEST.innerHTML = HTML;
 
@@ -27,17 +27,23 @@ $DEST.className = 'markdown-pre-js-out';
 $STYLE.rel = 'stylesheet';
 $STYLE.href = getStylesheetUrl();
 
-DOC.body.insertBefore($DEST, $PRE)
-DOC.body.appendChild($STYLE)
+DOC.body.insertBefore($DEST, $PRE);
+DOC.body.appendChild($STYLE);
 
-console.warn('markdown-pre:', $PRE, $DEST)
+// Was: console.warn('markdown-pre:', $PRE, $DEST);
 
-function getStylesheetUrl () {
-  const IS_CDN_TEST = /\?test=data-cdn/.test(window.location.href)
-  const $_SCRIPT = DOC.querySelector(IS_CDN_TEST ? 'script[ data-cdn-src ]' : 'script[ src *= markdown-pre ]')
+// ------------------------------
 
-  const SCRIPT_URL = $_SCRIPT.getAttribute(IS_CDN_TEST ? 'data-cdn-src' : 'src' )
-  const M_URL = SCRIPT_URL.match(/-pre@[^\.]*$/)
+function getStylesheetUrl() {
+  const IS_CDN_TEST = /\?test=data-cdn/.test(window.location.href);
+  const $_SCRIPT = DOC.querySelector(
+    IS_CDN_TEST ? 'script[ data-cdn-src ]' : 'script[ src *= markdown-pre ]'
+  );
+
+  const SCRIPT_URL = $_SCRIPT.getAttribute(
+    IS_CDN_TEST ? 'data-cdn-src' : 'src'
+  );
+  const M_URL = SCRIPT_URL.match(/-pre@[^.]*$/);
 
   return SCRIPT_URL + (M_URL ? '' : '/../..') + '/src/style.css';
 }
